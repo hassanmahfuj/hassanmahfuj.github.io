7901dcfc1780a369b5d124648bb0d8b8bcd38786
Hassan Mahfuj <itsmahfuj@gmail.com>
2024-02-28 22:23:57 +0600

    portfolio updated

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,38 +1,108 @@
 const items = [
   {
     imageSrc: "../image/hishabkitab.jpg",
-    title: "HISHAB KITAB",
+    title: "Hishab Kitab App",
     description:
       "Accounting app to know the financial situation with numerous essential features.",
     skills: ["Android", "Java"],
+    url: "https://play.google.com/store/apps/details?id=com.oshadharonlab.hk",
   },
   {
-    imageSrc: "../image/netflix.jpg",
-    title: "NETFLIX BANGLADESH WEBSITE",
+    imageSrc: "../image/woocommerce.jpg",
+    title: "Netflix Bangladesh Website",
     description:
       "A wordpress woocommerce website for selling netflix subscription in Bangladesh.",
     skills: ["Web Development", "WordPress", "WooCommerce"],
+    url: "https://www.netflixbangladesh.com",
   },
   {
     imageSrc: "../image/childrenssmile.jpg",
-    title: "CHILDREN'S SMILE FOUNDATION",
+    title: "Children's Smile Foundation",
     description:
       "To provide education, treatment, food for underprivileged, helpless and poor children.",
     skills: ["Java", "Android", "Firebase"],
+    url: "https://play.google.com/store/apps/details?id=com.oshadharonlab.childrenssmile",
   },
   {
     imageSrc: "../image/salat.jpg",
-    title: "SALAT TIME ANDROID APP",
+    title: "Salat Time Android App",
     description:
       "Get salat time by realtime location with this app. Don't kaza the Salat again.",
     skills: ["Java", "Android"],
+    url: "https://play.google.com/store/apps/details?id=com.oshadharonlab.salat",
   },
   {
     imageSrc: "../image/spondon.jpg",
-    title: "SPONDON BLOOD DONATION APP",
+    title: "Spondon Blood Donation App",
     description:
       "An application for managing donors information of a blood donation organization.",
     skills: ["Java", "Android", "Firebase"],
+    url: "https://play.google.com/store/apps/details?id=com.oshadharonlab.spondonreborn",
+  },
+  {
+    imageSrc: "../image/shopsense.png",
+    title: "Multi-Vendor eCommerce Solution",
+    description:
+      "This project aims to create a comprehensive online marketplace platform that allows multiple vendors to showcase and sell their products. The solution utilizes the Spring framework for robust backend development, Angular for building a dynamic and responsive web interface, and Flutter for crafting a seamless and engaging mobile app. This combination of technologies ensures efficient management of vendors, user-friendly web browsing, and a consistent experience across both web and mobile platforms.",
+    skills: [
+      "Java",
+      "SpringBoot",
+      "MySQL",
+      "Angular",
+      "Flutter",
+      "Dart",
+      "TypeScript",
+      "SQL",
+    ],
+    url: "https://github.com/hassanmahfuj/Multi-Vendor-eCommerce-Solution-with-Spring-Angular-Flutter",
+  },
+  {
+    imageSrc: "../image/hr-glance.png",
+    title: "Human Resource Management System",
+    description:
+      "HR Management System is software which satisfies the needs of the Human Resources Department of a company to manage employees' personal data (citizen identity number, name, surname, birthdate, educational information etc.), timings, holidays, annual leaves, payroll and so on. My HR Management System will meet the needs for managing the personal data, attendance, leaves, payroll of an employee.",
+    skills: ["Java", "Desktop App", "MySQL", "SQL"],
+    url: "https://github.com/hassanmahfuj/HR-Glance",
+  },
+  {
+    imageSrc: "../image/nodemcu.webp",
+    title: "ESP8266 WiFi Switching Solution",
+    description:
+      "The project aims to simplify the process of switching between Access Point (AP) and Station (STA) modes on the ESP8266 NodeMCU module, making it more user-friendly and versatile for IoT and wireless networking applications. The project addresses the need for quick and hassle-free transitions between these modes, which are essential for IoT devices to connect to existing networks or establish their networks for other devices to connect.",
+    skills: ["Arduino", "C++", "IoT", "Microcontroller"],
+    url: "https://github.com/hassanmahfuj/ESP8266-Solution-For-Easy-WiFi-Connection",
+  },
+  {
+    imageSrc: "../image/snowball.png",
+    title: "Snowball Showdown - Online Game",
+    description:
+      "This is a thrilling two-player web game where you take on the role of either the red or blue player. Move your character vertically and throw snowballs at your opponent to score points. The first player to reach 15 points wins the game. With strategic movement and precise aim, it's a test of reflexes and accuracy in a fun snowball battle.",
+    skills: ["HTML", "CSS", "JavaScript", "Game Development"],
+    url: "https://hassanmahfuj.github.io/Snowball-Showdown-Online-Game",
+  },
+  {
+    imageSrc: "../image/chatapp.png",
+    title: "ChaterApp Android",
+    description:
+      "This open-source Android project leverages Firebase as a real-time database to create a feature-rich chat application with a strong emphasis on user privacy and simplicity. The core functionality of the app allows users to send messages, share their real-time location, and authenticate using their phone number.",
+    skills: ["Android", "Firebase"],
+    url: "https://github.com/hassanmahfuj/ChaterApp-Android-Firebase",
+  },
+  {
+    imageSrc: "../image/chatappweb.jpg",
+    title: "Realtime Chat Web Application",
+    description:
+      "This is a real-time chat application with a client in the browser developed using Angular for the frontend and Spring Boot for the backend. The backend uses WebSocket (via Spring's WebSocket support) for real-time communication between users. It also integrates with MySQL to store user data and chat history. Users can register with their email, first name, and last name, and then initiate chats with other registered users. The application also supports viewing and deleting old chats and allows users to sign out.",
+    skills: ["Web Development", "Angular", "SpringBoot", "WebSocket"],
+    url: "https://github.com/hassanmahfuj/Realtime-Chat-Application-with-SpringBoot-and-Angular",
+  },
+  {
+    imageSrc: "../image/nestiverse.png",
+    title: "Nestiverse - Rent Your Apartments App",
+    description:
+      "Nestiverse provide a platform for people to rent out their homes, apartments, or other types of accommodations to travelers and tourists.",
+    skills: ["Firebase", "Android", "Flutter"],
+    url: "https://github.com/hassanmahfuj/Nestiverse-Rent-Your-Apartments-Flutter-App",
   },
 ];
 
@@ -58,8 +128,8 @@ function appendItems(items) {
                 <img src="${item.imageSrc}" />
             </div>
             <div class="p-item-end">
-                <h1>${item.title}</h1>
-                <p>${item.description}</p>
+              <h1><a href="${item.url}" target="_blank">${item.title}</a></h1>
+              <p>${item.description}</p>
             </div>
         `;
 
